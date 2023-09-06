ccc487207dc51e97bbccd3ae24b4751aee0ed96e
Yaron <horsky.yaron@gmail.com>
2023-09-06 15:54:10 +0300

    aoc 1 done

---
+++ 1/go/sol.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
+
+func main() {
+
+	log.SetPrefix("ERROR: ")
+	log.SetFlags(0)
+
+	file, err := os.Open("../file")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
+
+	max := 1
+	elf_idx := 1
+	counter := 1
+
+	for scanner.Scan() {
+		idx := counter
+		total := 0
+		num := scanner.Text()
+		for num != "" {
+			temp, err := strconv.Atoi(num)
+			if err != nil {
+				log.Fatal(err)
+			}
+			total += temp
+			scanner.Scan()
+			num = scanner.Text()
+		}
+		if total > max {
+			max = total
+			elf_idx = idx
+			counter++
+		}
+		fmt.Printf("elf %d with %d calories!\n", idx, total)
+		counter++
+	}
+
+	fmt.Printf("MVP: elf %d with %d calories!\n", elf_idx, max)
+}
